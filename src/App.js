cd8b8b494d4820109617990a634a780716e8dbfe
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-09-29 19:10:00 +0700

    auth slice

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
+
 
 function App() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+  console.log(isLoggedIn);
   return (
     <div className="App">
       <Auth />
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+const { configureStore } = require("@reduxjs/toolkit");
+const { default: authSlice } = require("./auth-slice");
+
+const store = configureStore({
+    reducer:{
+        auth:authSlice.reducer
+    }
+})
+export default store;
