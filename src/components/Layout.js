2d28f76ee27db7984cc58497081cba84a2cddc31
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-10-04 19:03:04 +0700

    test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,8 +5,14 @@ import "./Layout.css";
 import CartItems from "./CartItems";
 import { useSelector } from "react-redux";
 const Layout = () => {
-  let total = 100;
   const showCart = useSelector(state => state.cart.showCart);
+  let total = 0;
+  const datas = useSelector(state => state.cart.itemList)
+  
+  for(let i = 0 ; i < datas.length ; i++){
+    console.log(` data ke ${i} = ${ datas[i].quantity}`)
+    total += datas[i].quantity * datas[i].price
+  }
   return (
     <React.Fragment>
       <div className="layout">
